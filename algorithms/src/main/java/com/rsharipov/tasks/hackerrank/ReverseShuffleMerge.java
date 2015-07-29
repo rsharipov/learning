ea7242b12f4e303a95eb39ff56206842e5a814e9
Renat Sharipov <rsharipov@appliedtech.ru>
2015-07-29 13:26:00 +0500

    Started solving ReverseShuffleMerge task

---
+++ algorithms/src/main/java/com/rsharipov/tasks/hackerrank/ReverseShuffleMerge.java
@@ -0,0 +1,80 @@
+package com.rsharipov.tasks.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseShuffleMerge {
+
+    public int[] countCharsAndDivideInHalf(String line) {
+        int counts[] = new int[26];
+        for (int i = 0; i < line.length(); ++i) {
+            counts[line.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < counts.length; ++i) {
+            counts[i] /= 2;
+        }
+        return counts;
+    }
+    
+    public int sumArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; ++i) {
+            sum += array[i];
+        }
+        return sum;
+    }
+    
+    public int findLeftmostEndOfLineWithCharsExclusive(
+            String line, int[] charsCounts, int sum) {
+        int countFromEnd[] = new int[26];
+        int start = 0;
+        while (sum > 0) {
+            int ch = line.charAt(start);
+            if (countFromEnd[ch - 'a'] < charsCounts[ch - 'a']) {
+                --sum;
+                countFromEnd[ch - 'a']++;
+            }
+            ++start;
+        }
+        return start;
+    }
+    
+    public String solve(String line) {
+        int[] counts = countCharsAndDivideInHalf(line);
+        int start = findLeftmostEndOfLineWithCharsExclusive(line, counts, sumArray(counts));
+        return findGreatestReverse(line, start, counts);
+    }
+
+    public String findGreatestReverse(String line, int start, int[] counts) {
+        int end = line.length();
+        StringBuilder result = new StringBuilder();
+        int toSkip[] = new int[26];
+        for (int j = 0; j < line.length(); ++j) {
+            while (start > 0 && toSkip[line.charAt(start - 1) - 'a'] > 0) {
+                toSkip[line.charAt(start - 1) - 'a']--;
+                --start;
+            }
+            for (int ch = 0; ch < 26; ++ch) {
+                if (counts[ch] > 0) {
+                    int index = line.lastIndexOf((char)('a' + ch), end - 1);
+                    if (index > -1 && index >= start - 1) {
+                        System.out.println("Adding " + index);
+                        result.append((char)(ch + 'a'));
+                        --counts[ch];
+                        end = index;
+                        toSkip[ch]++;
+                        break;
+                    }                    
+                }
+            }            
+        }
+        return result.toString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        System.out.println(new ReverseShuffleMerge().solve(line));
+    }
+}
---
+++ algorithms/src/test/java/com/rsharipov/tasks/hackerrank/ReverseShuffleMergeTest.java
@@ -0,0 +1,72 @@
+package com.rsharipov.tasks.hackerrank;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ReverseShuffleMergeTest {
+    
+    @Test
+    public void testCountChars() {
+        String line = "bdabaceadaedaaaeaecdeadababdbeaeeacacaba";
+        ReverseShuffleMerge instance = new ReverseShuffleMerge();
+        int[] expResult = new int[] { 8, 3, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        int[] result = instance.countCharsAndDivideInHalf(line);
+        assertArrayEquals(expResult, result);
+    }
+
+    @Test
+    public void testSumArray() {
+        int[] array = new int[] { 8, 3, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        ReverseShuffleMerge instance = new ReverseShuffleMerge();
+        int expResult = 20;
+        int result = instance.sumArray(array);
+        assertEquals(expResult, result);
+    }
+
+//    @Test
+//    public void testFindRightmostStartOfLineWithChars() {
+//        String line = "bdabaceadaedaaaeaecdeadababdbeaeeacacaba";
+//        int[] charsCounts = new int[] { 8, 3, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+//        };
+//        int sum = 20;
+//        ReverseShuffleMerge instance = new ReverseShuffleMerge();
+//        int expResult = 19;
+//        int result = instance.findRightmostStartOfLineWithChars(line, charsCounts, sum);
+//        assertEquals(expResult, result);
+//    }
+
+    /**
+     * Test of solve method, of class ReverseShuffleMerge.
+     */
+    @Test
+    public void testSolve() {
+        String line = "eggegg";
+        ReverseShuffleMerge instance = new ReverseShuffleMerge();
+        String expResult = "egg";
+        String result = instance.solve(line);        
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of solve method, of class ReverseShuffleMerge.
+     */
+    @Test
+    public void testSolveBigLine() {
+        String line = "bdabaceadaedaaaeaecdeadababdbeaeeacacaba";
+        ReverseShuffleMerge instance = new ReverseShuffleMerge();
+        String expResult = "aaaaaabaaceededecbdb";
+        System.out.println(expResult);
+        String result = instance.findGreatestReverse(line, 19, 
+            new int[] { 8, 3, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        });
+        System.out.println(result);
+        assertEquals(expResult, result);
+    }
+    
+}
